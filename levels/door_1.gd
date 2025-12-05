4733313ea292ac51aa9634288761498e57bc058c
rutheral-cpu <ruther.al@wcs-g.com>
2025-12-05 13:52:52 -0700

    idkwhydoordoesntwork

--- levels/door_1.gd
+++ levels/door_1.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var player: Node2D = $"../../Player"
+@onready var playertrigger: Area2D = $"../../playertrigger"
 
 @onready var playerco := get_tree().get_nodes_in_group("player")[0]
 
@@ -9,8 +10,8 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if player.dooropen:
-		if playerco.direction:
+	if playertrigger.dooropen1 == true:
+		if playerco.velocity.x < 0.0:
 			scale.x = -2.0
 		else:
 			scale.x = 2.0
