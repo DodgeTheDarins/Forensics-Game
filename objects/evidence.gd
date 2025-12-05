a7cc889bb1cab2e4ce39ba0bfd8fee4806a7ea0f
Jeremy Shields <Jer@POWERHOUSEPOINT.COM>
2025-12-05 10:27:00 -0700

    evidence works now

--- objects/evidence.gd
+++ objects/evidence.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 signal collected(item_name)
-
+var in_evidence
 @export var item_name: String = "Evidence"
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and Input.is_action_just_pressed("pickup"):
+func _on_area_2d_body_entered(body: Node2D):
+	if body.is_in_group("player"):
+		in_evidence = true
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("pickup") and in_evidence:
 		emit_signal("collected", item_name)
 		queue_free() # removes the item
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	in_evidence = false
