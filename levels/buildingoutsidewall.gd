3811e862a5e1c7f165e5bed070c35fb755af193f
Jeremy Shields <Jer@POWERHOUSEPOINT.COM>
2025-12-08 11:33:00 -0700

    light and building fix

--- levels/buildingoutsidewall.gd
+++ levels/buildingoutsidewall.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @onready var player := get_tree().get_nodes_in_group("player")[0]
+@onready var light1: PointLight2D = $"../PointLight2D"
+@onready var light2: PointLight2D = $"../PointLight2D2"
 
 
 func _ready() -> void:
@@ -16,9 +18,12 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		visible = false
-
+		light1.visible = true
+		light2.visible = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		visible = true
+		light1.visible = false
+		light2.visible = false
