abb7f32ec1000247f43570bf77bbdcfb69fcdd37
Jeremy Shields <Jer@POWERHOUSEPOINT.COM>
2025-12-10 13:58:46 -0700

    e

--- scripts/tableinspectmode.gd
+++ scripts/tableinspectmode.gd
@@ -1,34 +1,57 @@
 extends Node2D
 
-@export var inventory: Node = null
-var slot_nodes: Array = []
+# grab the 8 sprite spots
+@onready var spots := [
+	$Spot1, $Spot2, $Spot3, $Spot4,
+	$Spot5, $Spot6, $Spot7, $Spot8
+]
+
+# internal → visible name
+var aliases := {
+	"knife_clean": "knife",
+	"knife_tampered": "knife",
+	"knife_bloody": "knife",
+	"gloves": "gloves",
+	"blood": "blood"
+}
+
+# map visible names to textures
+var textures := {
+	"knife": preload("res://ui/knife_clean.png"),
+	"gloves": preload("res://levels/download.jpg"),
+	"blood": preload("res://levels/blood_splatter-removebg-preview.png"),
+	"knife_tampered":preload("uid://dtbdugx1y42ov"),
+	"knife_bloody": preload("uid://bawuxx2rqlaa8")}
+	
 
 func _ready():
-	# Cache all Sprite2D children as "slots"
-	for c in get_children():
-		if c is Sprite2D:
-			slot_nodes.append(c)
+	var inv = get_node("/root/inventory")
+	inv.connect("item_added", Callable(self, "_refresh"))
+	inv.connect("item_equipped", Callable(self, "_refresh"))
+	inv.connect("item_unequipped", Callable(self, "_refresh"))
+	_refresh()
 
-	update_slots()
+func _refresh(_junk = null):
+	var inv = get_node("/root/inventory")
+	var items: Array = inv.get_items()
 
-func update_slots():
-	if inventory == null:
-		return
+	# Clear all spots
+	for s in spots:
+		s.texture = null
+		s.visible = false
 
-	var items = inventory.get_items()
+	var index := 0
 
-	# Loop through all 8 slots
-	for i in range(slot_nodes.size()):
-		var slot = slot_nodes[i]
+	for internal_name in items:
+		if index >= spots.size():
+			break
 
-		if i < items.size():
-			var item = items[i]
-			var icon_path = item["icon"]
+		var visual_name = internal_name
+		if internal_name in aliases:
+			visual_name = aliases[internal_name]
 
-			# Load icon and show it
-			slot.texture = load(icon_path)
-			slot.visible = true
-		else:
-			# No item for this slot
-			slot.texture = null
-			slot.visible = false
+		if visual_name in textures:
+			spots[index].texture = textures[visual_name]
+			spots[index].visible = true
+
+		index += 1
